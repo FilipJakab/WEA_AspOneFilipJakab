5cb6cf65a4e657d20a0766cd9469e144db909587
filipJakab <jakabfilip.fj@gmail.com>
2018-05-25 19:47:56 +0200

    Initial commit

---
+++ WEA_AspOneFilipJakab/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WEA_AspOneFilipJakab.MvcApplication" Language="C#" %>
---
+++ WEA_AspOneFilipJakab/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(WEA_AspOneFilipJakab.Startup))]
+
+namespace WEA_AspOneFilipJakab
+{
+	public partial class Startup
+	{
+		public void Configuration(IAppBuilder app)
+		{
+			ConfigureAuth(app);
+		}
+	}
+}
