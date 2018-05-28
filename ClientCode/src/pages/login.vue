368c032e1569f5b6f6955925b9eb640da32697d7
filipJakab <jakabfilip.fj@gmail.com>
2018-05-28 23:43:20 +0200

    Created simple-webpack client-side js framework with MaterializeCss

---
+++ ClientCode/src/pages/login.vue
@@ -0,0 +1,43 @@
+<template>
+	<div class="container">
+		<div class="row">
+			<div class="col s4 offset-s4 container" id="form">
+				<div class="row">
+					<div class="input-field col s12">
+						<input type="text" id="Email" v-model="email">
+						<label for="Email">Email</label>
+					</div>
+					<div class="input-field col s12">
+						<input type="password" id="password" v-model="password">
+						<label for="password">Password</label>
+					</div>
+					<button class="btn waves-effect waves-light" @click="onLoginSubmited">
+						Submit
+					</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	data () {
+		return {
+			email: "",
+			password: ""
+		}
+	},
+	methods: {
+		onLoginSubmited() {
+
+		}
+	}
+}
+</script>
+
+<style lang="scss">
+	#form {
+		margin-top: 80px;
+	}
+</style>
